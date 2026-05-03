697327c021becbf9d25c725974ca55053c8dee3e
adityaawate23-jpg <aditya.awate23@vit.edu>
2026-05-03 21:12:34 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="landing">
+      <h1>Paradise Nursery</h1>
+      <p>Your Learning Management Platform</p>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
