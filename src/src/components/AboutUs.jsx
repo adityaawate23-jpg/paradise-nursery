2738bd97446a9032304d32f8995c9f25662f1e39
adityaawate23-jpg <aditya.awate23@vit.edu>
2026-05-03 21:15:10 +0530

    Create AboutUs.jsx

---
+++ src/src/components/AboutUs.jsx
@@ -0,0 +1,12 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>Paradise Nursery</h1>
+      <p>
+        Welcome to Paradise Nursery. We provide beautiful indoor plants to make your home fresh and lively.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
